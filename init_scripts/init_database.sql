c99ce5730f20fce3167cba89687de0cf03d9b955
tamasandor <tamas.andor@yahoo.com>
2026-02-12 00:41:42 +0100

    fix: remove init script for the database (JPA should handle it for us)

--- init_scripts/init_database.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    userID INT PRIMARY KEY,
-    username VARCHAR(255) UNIQUE,
-    email VARCHAR(255) UNIQUE,
-    password VARCHAR(255),
-    name VARCHAR(255),
-    bio TEXT,
-    profilePicture VARCHAR(255)
-);
